793c7417d9a28a9277b6ed93438f08cabb7fa0b2
M3RCX <mercante243@gmail.com>
2020-09-09 22:55:31 -0300

    modal pronto

--- js/script.js
+++ js/script.js
@@ -1,4 +1,44 @@
-$(function(){
+/*
+function modalMoni(modalM){
+  const modal = document.getElementById(modalM);
+  modal.addEventListener('click', function(){
+    console.log("to aqui")
+    
+  });
+};
 
-    $("#antes img:eq(0)").addClas("ativo").show();
+const mm = document.querySelector('.btnModal');
+mm.addEventListener('click', function(){ 
+  modalMoni('modal-moni')
+});
+*/
+
+document.addEventListener('DOMContentLoaded', () => {
+  const Modal = document.getElementById('modal-moni');
+  const Active = document.getElementById('ModalM');
+  const close = document.getElementById('close')
+  
+  Active.addEventListener('click', () => {
+          Modal.classList.add("show");
+  });
+  
+  close.addEventListener('click', () =>{
+    Modal.classList.remove("show");
+  });
+
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const Modal = document.getElementById('modal-atelie');
+  const Active = document.getElementById('ModalA');
+  const close = document.getElementById('Fechar')
+  
+  Active.addEventListener('click', () => {
+          Modal.classList.add("show");
+  });
+  
+  close.addEventListener('click', () =>{
+    Modal.classList.remove("show");
+  });
+  
 });
